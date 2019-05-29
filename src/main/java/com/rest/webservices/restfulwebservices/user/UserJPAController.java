254e0c76379dc333bc63499675e5e0c5b4779e51
ganjali <gaddam.anjali@oracle.com>
2019-05-29 23:28:50 +0530

    add Post resourse and relationship between user and post, restful services with post

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAController.java
@@ -24,8 +24,11 @@ public class UserJPAController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/jpa/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return userRepository.findAll();
     }
 
@@ -38,10 +41,10 @@ public class UserJPAController {
 //    }
 
     @GetMapping("/jpa/users/{id}")
-    public Resource<User> retrieveUser(@PathVariable int id){
+    public Resource<User> retrieveUser(@PathVariable int id) {
         Optional<User> user = userRepository.findById(id);
-        if(!user.isPresent())
-            throw new UserNotFoundException("id-"+id);
+        if (!user.isPresent())
+            throw new UserNotFoundException("id-" + id);
 
         //HATEOAS user with links
         Resource<User> resource = new Resource<User>(user.get());
@@ -51,7 +54,7 @@ public class UserJPAController {
     }
 
     @PostMapping("/jpa/users")
-    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = userRepository.save(user);
         // /user/{id}
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
@@ -59,7 +62,31 @@ public class UserJPAController {
     }
 
     @DeleteMapping("/jpa/users/{id}")
-    public void deleteUser(@PathVariable int id){
+    public void deleteUser(@PathVariable int id) {
         userRepository.deleteById(id);
+    }
+
+    @GetMapping("/jpa/users/{id}/posts")
+    public List<Post> retrieveAllUsers(@PathVariable int id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if(!userOptional.isPresent()){
+            throw  new UserNotFoundException("id-"+id);
+        }
+
+        return userOptional.get().getPosts();
+    }
+
+    @PostMapping("/jpa/users/{id}/posts")
+    public ResponseEntity<Object> createPost(@PathVariable int id, @RequestBody Post post) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if(!userOptional.isPresent()){
+            throw  new UserNotFoundException("id-"+id);
+        }
+        User user = userOptional.get();
+        post.setUser(user);
+        Post savedPost = postRepository.save(post);
+        // /user/{id}
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedPost.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
